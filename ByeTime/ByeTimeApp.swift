3cd9f66154bea09fe772545f026f65f56693bc2c
Lian <176339768+Lian735@users.noreply.github.com>
2026-01-21 23:14:45 +0100

    Initial Commit

---
+++ ByeTime/ByeTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ByeTimeApp.swift
+//  ByeTime
+//
+//  Created by Lian on 21.01.26.
+//
+
+import SwiftUI
+
+@main
+struct ByeTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
